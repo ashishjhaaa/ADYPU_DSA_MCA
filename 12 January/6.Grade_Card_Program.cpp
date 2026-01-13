4053f696d5ecaaef358693067425da95a1dc0488
Ashish Jha <ashish.jhaaaaa@gmail.com>
2026-01-13 20:18:03 +0530

    Add files via upload

---
+++ 12 January/6.Grade_Card_Program.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int marks;
+    cin >> marks;
+
+    if (marks >= 90)
+    {
+        cout << "A";
+    }
+    else if (marks >= 75)
+    {
+        cout << "B";
+    }
+    else if (marks >= 60)
+    {
+        cout << "C";
+    }
+    else if (marks >= 40)
+    {
+        cout << "D";
+    }
+    else
+    {
+        cout << "Fail";
+    }
+    return 0;
+}
